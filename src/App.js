81f93910d2c5ff2f80ce64ae4111107dde73035b
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-07-14 13:06:05 +0530

    gif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import SearchForm from "./SearchForm";
+import GifList from "./GifList";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("webdeveloper");
+  const [isloading, setIsLoading] = useState(true);
+
+  const performSearch = (value) => setQuery(value);
+  // fetching data from giphy api
+  useEffect(() => {
+    axios(
+      `https://api.giphy.com/v1/gifs/search?api_key=f3b8PiHqEC58PxYkUN0KluDjNK6pTufN&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((res) => setData(res.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">Gif</h1>
+          <SearchForm onSearch={performSearch} />
+        </div>
+      </div>
+      <div className="main-content">
+        {isloading ? <p>Loading...</p> : <GifList data={data} />}
+      </div>
+    </>
   );
 }
 
